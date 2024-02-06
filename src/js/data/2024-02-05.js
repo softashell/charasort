78a561a938f9e499bcdefc613cc4a5cceb51111d
soft as HELL <softashell@cock.li>
2024-02-06 02:57:01 +0200

    Quick hack

---
+++ src/js/data/2024-02-05.js
@@ -0,0 +1,281 @@
+dataSetVersion = "2024-02-05"; // Change this when creating a new data set version. YYYY-MM-DD format.
+dataSet[dataSetVersion] = {};
+
+dataSet[dataSetVersion].options = [
+  {
+    name: "Filter by Series Entry",
+    key: "series",
+    tooltip: "Check this to restrict to certain series.",
+    checked: false,
+    sub: [
+      { name: "Puggers", key: "pug" }
+    ]
+  }
+];
+
+dataSet[dataSetVersion].characterData = [
+  {
+    name: "Sunabozu",
+    img: "hx1qvj.png",
+    opts: {
+      series: ["pug"]
+    }
+  },
+  {
+    name: "Oni",
+    img: "hx1qvj.png",
+    opts: {
+      series: ["pug"]
+    }
+  },
+  {
+    name: "Infrequent",
+    img: "hx1qvj.png",
+    opts: {
+      series: ["pug"]
+    }
+  },
+  {
+    name: "Halcyon",
+    img: "hx1qvj.png",
+    opts: {
+      series: ["pug"]
+    }
+  },
+  {
+    name: "Wahaha",
+    img: "hx1qvj.png",
+    opts: {
+      series: ["pug"]
+    }
+  },
+  {
+    name: "Rain",
+    img: "hx1qvj.png",
+    opts: {
+      series: ["pug"]
+    }
+  },
+  {
+    name: "Kudegra",
+    img: "hx1qvj.png",
+    opts: {
+      series: ["pug"]
+    }
+  },
+  {
+    name: "Jef",
+    img: "hx1qvj.png",
+    opts: {
+      series: ["pug"]
+    }
+  },
+  {
+    name: "Agiel",
+    img: "hx1qvj.png",
+    opts: {
+      series: ["pug"]
+    }
+  },
+  {
+    name: "Persica",
+    img: "hx1qvj.png",
+    opts: {
+      series: ["pug"]
+    }
+  },
+  {
+    name: "Nevra",
+    img: "hx1qvj.png",
+    opts: {
+      series: ["pug"]
+    }
+  },
+  {
+    name: "Kinoko",
+    img: "hx1qvj.png",
+    opts: {
+      series: ["pug"]
+    }
+  },
+  {
+    name: "Neotokyoscrub",
+    img: "hx1qvj.png",
+    opts: {
+      series: ["pug"]
+    }
+  },
+  {
+    name: "Milk",
+    img: "hx1qvj.png",
+    opts: {
+      series: ["pug"]
+    }
+  },
+  {
+    name: "Shazam",
+    img: "hx1qvj.png",
+    opts: {
+      series: ["pug"]
+    }
+  },
+  {
+    name: "blaberry",
+    img: "hx1qvj.png",
+    opts: {
+      series: ["pug"]
+    }
+  },
+  {
+    name: "Puddy",
+    img: "hx1qvj.png",
+    opts: {
+      series: ["pug"]
+    }
+  },  {
+    name: "Rusher",
+    img: "hx1qvj.png",
+    opts: {
+      series: ["pug"]
+    }
+  },  {
+    name: "Miso",
+    img: "hx1qvj.png",
+    opts: {
+      series: ["pug"]
+    }
+  },
+  {
+    name: "XX",
+    img: "hx1qvj.png",
+    opts: {
+      series: ["pug"]
+    }
+  },
+  {
+    name: "Phorce",
+    img: "hx1qvj.png",
+    opts: {
+      series: ["pug"]
+    }
+  },
+  {
+    name: "ningurist",
+    img: "hx1qvj.png",
+    opts: {
+      series: ["pug"]
+    }
+  },
+  {
+    name: "Dommler",
+    img: "hx1qvj.png",
+    opts: {
+      series: ["pug"]
+    }
+  },
+  {
+    name: "Nutnoze",
+    img: "hx1qvj.png",
+    opts: {
+      series: ["pug"]
+    }
+  },
+  {
+    name: "Masterkatze",
+    img: "hx1qvj.png",
+    opts: {
+      series: ["pug"]
+    }
+  },
+  {
+    name: "Shinra",
+    img: "hx1qvj.png",
+    opts: {
+      series: ["pug"]
+    }
+  },
+  {
+    name: "3LOW",
+    img: "hx1qvj.png",
+    opts: {
+      series: ["pug"]
+    }
+  },
+  {
+    name: "Hosomi",
+    img: "hx1qvj.png",
+    opts: {
+      series: ["pug"]
+    }
+  },
+  {
+    name: "Devastator",
+    img: "hx1qvj.png",
+    opts: {
+      series: ["pug"]
+    }
+  },
+  {
+    name: "Marterzon",
+    img: "hx1qvj.png",
+    opts: {
+      series: ["pug"]
+    }
+  },
+  {
+    name: "Talibus",
+    img: "hx1qvj.png",
+    opts: {
+      series: ["pug"]
+    }
+  },
+  {
+    name: "Hyacinth",
+    img: "hx1qvj.png",
+    opts: {
+      series: ["pug"]
+    }
+  },
+  {
+    name: "Boon",
+    img: "hx1qvj.png",
+    opts: {
+      series: ["pug"]
+    }
+  },
+  {
+    name: "Ace of Hearts",
+    img: "hx1qvj.png",
+    opts: {
+      series: ["pug"]
+    }
+  },
+  {
+    name: "DRT",
+    img: "hx1qvj.png",
+    opts: {
+      series: ["pug"]
+    }
+  },
+  {
+    name: "Average",
+    img: "hx1qvj.png",
+    opts: {
+      series: ["pug"]
+    }
+  },
+  {
+    name: "Glitcher",
+    img: "hx1qvj.png",
+    opts: {
+      series: ["pug"]
+    }
+  },
+  {
+    name: "Serbian guy",
+    img: "hx1qvj.png",
+    opts: {
+      series: ["pug"]
+    }
+  }
+];
