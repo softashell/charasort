6138b6c3d625f47a78eb97956ab543bd6499b5f4
soft as HELL <softashell@cock.li>
2024-02-07 19:24:34 +0200

    Default to 0 images

--- src/js/main.js
+++ src/js/main.js
@@ -466,9 +466,9 @@ function progressBar(indicator, percentage) {
 /**
  * Shows the result of the sorter.
  * 
- * @param {number} [imageNum=3] Number of images to display. Defaults to 3.
+ * @param {number} [imageNum=0] Number of images to display. Defaults to 3.
  */
-function result(imageNum = 3) {
+function result(imageNum = 0) {
   document.querySelectorAll('.finished.button').forEach(el => el.style.display = 'block');
   document.querySelector('.image.selector').style.display = 'block';
   document.querySelector('.time.taken').style.display = 'block';
